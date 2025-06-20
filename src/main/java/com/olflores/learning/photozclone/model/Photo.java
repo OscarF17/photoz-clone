3f8181334027896bc80648eb0b8f4a22bfd78c89
Oscar Luciano Flores Leija <oscar.l.flores@oracle.com>
2025-06-20 10:41:35 -0600

    finished prototype

---
+++ src/main/java/com/olflores/learning/photozclone/model/Photo.java
@@ -0,0 +1,56 @@
+package com.olflores.learning.photozclone.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.validation.constraints.NotEmpty;
+
+public class Photo {
+    private String id;
+
+    @NotEmpty
+    private String filename;
+
+    private String contentType;
+
+    @JsonIgnore
+    private byte[] data;
+
+    public Photo() {
+    }
+
+    public Photo(String id, String filename) {
+        this.id = id;
+        this.filename = filename;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getContentType() {
+        return contentType;
+    }
+
+    public void setContentType(String contentType) {
+        this.contentType = contentType;
+    }
+
+    public String getFilename() {
+        return filename;
+    }
+
+    public void setFilename(String filename) {
+        this.filename = filename;
+    }
+
+    public byte[] getData() {
+        return data;
+    }
+
+    public void setData(byte[] data) {
+        this.data = data;
+    }
+}
